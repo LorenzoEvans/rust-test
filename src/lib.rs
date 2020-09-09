d00f6db2eb63cbd07b10a666a3581e10de54e519
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-09-08 20:43:15 -0700

    Adding messages, their corresponding update functions, and comments

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,24 @@ use seed::*;
 #[derive(Default)]
 struct Model {
     text_to_show: String,
-    // saved_items: Vec<String> // Vector to hold items.
-                                // index placements will be identifiers for deletion.
-
+    selected_item: ItemId,
+    pub saved_items: Vec<String> // Vector to hold items.
+    // index placements will be identifiers for deletion.
+    
 }
-
+#[derive(Default)]
+struct ItemId {
+    id: i32
+}
 #[derive(Clone)]
 enum Msg {
     ChangeText(String),
     // We need a message to indicate clearing state
-    // Clear,
+    Clear,
     // We need a message to indicate deleting a single item
-    // Delete(i32)
+    Delete(usize),
     // We need a message to indicate saving a single item.
-    // SaveText(String)
+    SaveItem(String)
 }
 
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
@@ -25,9 +29,25 @@ fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
 
     match msg {
         ChangeText(new_text) => model.text_to_show = new_text,
-        // ClearMessages
-        // DeleteItem
-        // SaveItem
+        Clear => {
+            let reset_vector: Vec<String> = Vec::new();
+            // Create a new vector.
+            model.saved_items = reset_vector;
+            // Set old vector value to new empty vector.
+        }
+        Delete(item_id) => {
+
+            let mut update_vec = model.saved_items.clone();
+            // Clone the old saved items vector off the heap, so as to
+            // not mutate state directly.
+            update_vec.remove(item_id);
+            // Remove desired item from the update vector based on index.
+            model.saved_items = update_vec;
+            // Set old vector to new update vector.
+        }
+        SaveItem(string) => {
+            model.saved_items.push(string);
+        }
     }
 }
 
